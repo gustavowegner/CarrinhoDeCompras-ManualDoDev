d9d3ced7695165e52854d74a31f78658a320343e
Gustavo Wegner <gustavowegnerferreira@gmail.com>
2023-09-11 22:19:36 -0300

    Subindo projeto.

---
+++ src/context/Provider.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import React from 'react';
+import propTypes from 'prop-types';
+import AppContext from './AppContext';
+
+function Provider({ children }) {
+
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const value = {
+    products, 
+    setProducts,
+    loading, 
+    setLoading,
+    cartItems, 
+    setCartItems,
+    isCartVisible, 
+    setIsCartVisible,
+  };
+
+  return ( 
+    <AppContext.Provider value={ value }>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export default Provider;
+
+Provider.propTypes = {
+  children: propTypes.any
+}.isRequired;
